d021eaf47af1bef827e9c84e9fad5d6ed1474062
Lukas Knoepfel <shylux@gmail.com>
2014-02-12 15:57:09 +0100

    Moved version in setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = open('collective/jqcookie/version.txt').read().strip()
+version = '1.0a4-dev'
 maintainer = 'Victor baumann'
 
 setup(name='collective.jqcookie',
